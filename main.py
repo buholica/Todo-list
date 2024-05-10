7324d8f91c265f3774b6b20fad0815ae6df11b09
Oksana <oxanachyrkova9@gmail.com>
2024-05-10 09:01:01 +0300

    Add messages for empty tasks list

--- main.py
+++ main.py
@@ -1,48 +1,34 @@
-from flask import Flask, render_template, request, redirect
-from flask_bootstrap import Bootstrap5
-# import requests
-# from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, flash
 import os
 from day import Day
 import json
 
-# load_dotenv("C:\\Users\\Oksana\\Desktop\\passwords.env.txt")
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
-Bootstrap5(app)
-
-# SHEETY_TOKEN = os.getenv("SHEETY_TOKEN_TODO_L")
-# DOC_ID = os.getenv("DOC_ID_TODO_L")
-# sheety_endpoint = "https://api.sheety.co/2af8e23237aae490a742fcae381a9e81/todoList/list1"
-
-# sheety_headers = {
-#     "Authorization": f"Bearer {SHEETY_TOKEN}",
-# }
 
 today = Day()
 
 
 @app.route('/', methods=['GET'])
 def homepage():
-    # sheety_response = requests.get(sheety_endpoint, headers=sheety_headers)
-    # data = sheety_response.json()["list1"]
     with open('data.json') as data_json:
         data = json.load(data_json)["list1"]
     all_tasks = [task for task in data]
-    print(all_tasks)
-    return render_template("index.html", tasks=all_tasks, day=today.day)
+
+    message = "Please, add a new task."
+
+    return render_template("index.html", tasks=all_tasks, day=today.day, message=message)
 
 
 @app.route('/active-tasks', methods=['GET'])
 def show_active_tasks():
-    # sheety_response = requests.get(sheety_endpoint, headers=sheety_headers)
-    # data = sheety_response.json()["list1"]
     with open('data.json') as data_json:
         data = json.load(data_json)["list1"]
     active_tasks = [task for task in data if task["status"] == "active"]
 
-    return render_template("index.html", tasks=active_tasks, day=today.day)
+    message = "There are no active tasks currently."
+
+    return render_template("index.html", tasks=active_tasks, day=today.day, message=message)
 
 
 @app.route("/completed_task", methods=['GET'])
@@ -51,7 +37,9 @@ def show_completed_tasks():
         data = json.load(data_json)
     completed_tasks = [task for task in data["list1"] if task["status"] == "completed"]
 
-    return render_template("index.html", tasks=completed_tasks, day=today.day)
+    message = "There are no completed tasks currently."
+
+    return render_template("index.html", tasks=completed_tasks, day=today.day, message=message)
 
 
 @app.route('/add_task', methods=['POST'])
@@ -59,14 +47,8 @@ def add_task():
     new_task = request.form.get('new-task')
     with open('data.json') as data_json:
         data = json.load(data_json)["list1"]
-    # sheety_data = {
-    #     "list1": {
-    #         "date": today.day,
-    #         "text": new_task,
-    #         "status": "active"
-    #     }
-    # }
-    sheety_data = {
+
+    new_data = {
             "id": len(data),
             "date": today.day,
             "description": new_task,
@@ -76,11 +58,10 @@ def add_task():
     with open('data.json') as data_json:
         data = json.load(data_json)
 
-    data["list1"].append(sheety_data)
+    data["list1"].append(new_data)
 
     with open('data.json', "w") as data_json:
         json.dump(data, data_json, indent=4, separators=(',', ': '))
-    # requests.post(sheety_endpoint, json=sheety_data, headers=sheety_headers)
 
     return redirect('/')
 
@@ -120,4 +101,4 @@ def remove_task():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
