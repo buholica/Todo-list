b497d18ccd5d5f487d523575e2c747fdffb3d7e0
Oksana <oxanachyrkova9@gmail.com>
2024-05-31 18:58:28 +0300

    Add Sheety API integration and flask session

--- main.py
+++ main.py
@@ -16,53 +16,75 @@ sheets_headers = {
 }
 
 
+def get_tasks_from_sheet():
+    """Function to get tasks from Google Sheets using Sheety API"""
+    response = requests.get(sheets_endpoint, headers=sheets_headers)
+    response.raise_for_status()
+    return response.json()["list1"]
+
+
 @app.route('/', methods=['GET'])
 def homepage():
-    sheets_response = requests.get(sheets_endpoint, headers=sheets_headers)
-    data = sheets_response.json()["list1"]
-    print(data)
+    data = get_tasks_from_sheet()
+
+    if "tasks" not in session:
+        session["tasks"] = []
+
+    tasks_set = set(session["tasks"])
+
+    for task in data:
+        if task["id"] in session["tasks"]:
+            tasks_set.add(task["id"])
+
+    session.modified = True
+
+    valid_tasks = [task for task in data if task["id"] in session["tasks"]]
 
     message = "Please, add a new task."
 
-    return render_template("index.html", tasks=data, day=today.day, message=message)
+    return render_template("index.html", tasks=valid_tasks, day=today.day, message=message)
 
 
 @app.route('/active-tasks', methods=['GET'])
 def show_active_tasks():
-    sheets_response = requests.get(sheets_endpoint, headers=sheets_headers)
-    data = sheets_response.json()["list1"]
+    data = get_tasks_from_sheet()
+
     active_tasks = [task for task in data if task["status"] == "active"]
+    valid_tasks = [task for task in active_tasks if task["id"] in session["tasks"]]
 
     message = "There are no active tasks currently."
 
-    return render_template("index.html", tasks=active_tasks, day=today.day, message=message)
+    return render_template("index.html", tasks=valid_tasks, day=today.day, message=message)
 
 
 @app.route("/completed_task", methods=['GET'])
 def show_completed_tasks():
-    sheets_get_response = requests.get(sheets_endpoint, headers=sheets_headers)
-    data = sheets_get_response.json()["list1"]
+    data = get_tasks_from_sheet()
 
     completed_tasks = [task for task in data if task["status"] == "completed"]
+    valid_tasks = [task for task in completed_tasks if task["id"] in session["tasks"]]
+
     message = "There are no completed tasks currently."
 
-    return render_template("index.html", tasks=completed_tasks, day=today.day, message=message)
+    return render_template("index.html", tasks=valid_tasks, day=today.day, message=message)
 
 
 @app.route('/add_task', methods=['POST'])
 def add_task():
-    sheets_get_response = requests.get(sheets_endpoint, headers=sheets_headers)
-    data = sheets_get_response.json()["list1"]
+    data = get_tasks_from_sheet()
 
     new_task = request.form.get('new-task')
     new_task = {
         "list1": {
-            "id": len(data) + 1,
+            "id": data[-1]["id"] + 1,
             "date": today.day,
             "description": new_task,
             "status": "active"
         }
     }
+
+    session["tasks"].append(new_task["list1"]["id"])
+    session.modified = True
 
     sheets_post_response = requests.post(sheets_endpoint, json=new_task, headers=sheets_headers)
     sheets_post_response.raise_for_status()
@@ -75,8 +97,7 @@ def update_status():
     completed_task_id = request.form.get('task_id')
     completed_task_id_int = int(completed_task_id)
 
-    sheets_get_response = requests.get(sheets_endpoint, headers=sheets_headers)
-    data = sheets_get_response.json()["list1"]
+    data = get_tasks_from_sheet()
 
     for task in data:
         if completed_task_id_int == task["id"]:
@@ -100,11 +121,12 @@ def remove_task():
     task_id = request.form.get('task_id')
     task_id_int = int(task_id)
 
-    sheets_get_response = requests.get(sheets_endpoint, headers=sheets_headers)
-    data = sheets_get_response.json()["list1"]
+    data = get_tasks_from_sheet()
 
     for task in data:
         if task_id_int == task["id"]:
+            session["tasks"].remove(task_id_int)
+            session.modified = True
             sheets_delete_response = requests.delete(sheets_endpoint + f"/{task_id_int}", headers=sheets_headers)
             sheets_delete_response.raise_for_status()
             break
